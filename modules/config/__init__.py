fd19cf69eecaf300c77f76109a091a56dccf4ebb
akulij <48361256+akulij@users.noreply.github.com>
2023-05-10 17:54:51 +0500

    Add qiwi token to config file

--- modules/config/__init__.py
+++ modules/config/__init__.py
@@ -1,11 +1,12 @@
 from pydantic import (
     BaseSettings,
     PostgresDsn,
-    Field
+    Field,
 )
 
 class Settings(BaseSettings):
     database_uri: PostgresDsn = Field(env="DATABASE")
+    qiwi_token: str = Field(env="QIWI_TOKEN")
 
     class Config:
         env_file = ".env"

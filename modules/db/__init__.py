da78f56758c144f34fb24229ecc073b2ce320f0d
akulij <48361256+akulij@users.noreply.github.com>
2023-05-11 19:11:15 +0500

    Reorder imports

--- modules/db/__init__.py
+++ modules/db/__init__.py
@@ -1,17 +1,20 @@
+from typing import Optional, AsyncIterator
+
+from sqlmodel import SQLModel, Field
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from modules.config import config
 
-engine = create_async_engine(config.database_uri)
 
-from sqlmodel.ext.asyncio.session import AsyncSession
-
-from typing import Optional
-
-from sqlmodel import Field, SQLModel
+async def get_session() -> AsyncIterator[AsyncSession]:
+   async with AsyncSession(engine) as session:
+       yield session
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     nickname: str
+
+engine = create_async_engine(config.database_uri)

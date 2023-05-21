d9a4dfcec46e3cfd63a431ed9c3614397f813397
akulij <48361256+akulij@users.noreply.github.com>
2023-05-21 21:03:57 +0500

    create main db class

--- modules/db/__init__.py
+++ modules/db/__init__.py
@@ -1,20 +1,24 @@
 from typing import Optional, AsyncIterator
 
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, select
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from modules.config import config
-
-engine = create_async_engine(config.database_uri)
-
-
-async def get_session() -> AsyncIterator[AsyncSession]:
-   async with AsyncSession(engine) as session:
-       yield session
+from modules.config import Settings
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     nickname: str
+
+
+class DB:
+    def __init__(self, config: Settings):
+        self.engine = create_async_engine(config.database_uri)
+
+    async def get_user_byid(self, id: int) -> User | None:
+        async with AsyncSession(self.engine) as session:
+            user = await session.get(User, id)
+
+        return user

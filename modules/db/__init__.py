417a5e6f9ce029a675e56dcdef1591b894dc1300
akulij <48361256+akulij@users.noreply.github.com>
2023-06-01 21:36:47 +0500

    add faq table

--- modules/db/__init__.py
+++ modules/db/__init__.py
@@ -1,6 +1,6 @@
 from typing import Optional, AsyncIterator
 
-from sqlmodel import SQLModel, Field, select
+from sqlmodel import SQLModel, Field, select, delete
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -11,6 +11,11 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     nickname: str
+
+class FAQ(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    question: str = Field(unique=True)
+    answer: str
 
 
 class DB:
@@ -27,3 +32,23 @@ class DB:
         async with AsyncSession(self.engine) as session:
             session.add(user)
             await session.commit()
+
+    async def append_faq(self, faq: FAQ):
+        async with AsyncSession(self.engine) as session:
+            session.add(faq)
+            await session.commit()
+
+    async def get_faq(self):
+        async with AsyncSession(self.engine) as session:
+            q = select(FAQ)
+            e = await session.exec(q)
+            faq = e.all()
+
+        return faq
+
+    async def delete_faqs(self):
+        async with AsyncSession(self.engine) as session:
+            q = delete(FAQ)
+            await session.exec(q)
+            await session.commit()
+

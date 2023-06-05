dca8f4aa4672d7151131017e628d37be4515d27b
Akulij <aakulij@gmail.com>
2023-06-05 23:12:57 +0500

    Add support chatid to env variable

--- modules/config/__init__.py
+++ modules/config/__init__.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     database_uri: PostgresDsn = Field(env="DATABASE")
     qiwi_token: str = Field(env="QIWI_TOKEN")
     bot_token: str = Field(env="BOTTOKEN")
+    support_chatid: int = Field(env="SUPPORT_CHATID")
 
     class Config:
         env_file = ".env"

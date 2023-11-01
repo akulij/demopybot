2af7e3c636479d963f2f3e51ac8f3433f87fdb4c
Алмаз Нуртдинов <akulij@Noutbuk-Almaz.local>
2023-11-01 06:05:04 +0300

    add new dialog inline keyboard

--- modules/keyboards/tools.py
+++ modules/keyboards/tools.py
@@ -16,3 +16,13 @@ async def get_delete_faqs(db: DB, user: None = None):
     faqs = await db.get_faq()
 
     return list(map(lambda f: InlineKeyboardButton(f.question, callback_data=f"delete_{f.id}"), faqs))
+
+# async def get_bots_ikbd(db: DB, user: UserV1):
+async def get_bots_ikbd(db: DB, user):
+    return [
+               InlineKeyboardButton("Добавление пабликов", callback_data="bots_add_chat"),
+               InlineKeyboardButton("Изменить никнейм", callback_data="bots_change_nickname"),
+               InlineKeyboardButton("Изменить название акаунта", callback_data="bots_change_username"),
+               InlineKeyboardButton("Изменить аватар", callback_data="bots_change_avatar"),
+               InlineKeyboardButton("Изменить описание", callback_data="bots_change_description"),
+           ]
